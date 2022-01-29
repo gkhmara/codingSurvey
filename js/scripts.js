9a7767af5cad819e26e682ce29e94430131bf63e
Greg Khmara <gkhmara@gmail.com>
2022-01-28 18:57:40 -0800

    create update for final test output.

--- js/scripts.js
+++ js/scripts.js
@@ -40,26 +40,78 @@ $(document).ready(function(){
     }
 
     //FINAL RESULTS TEST
-    if (soup === 'tomatoSoup' && hat === 'tallHat') {
+    if (soup === 'tomatoSoup' && hat === 'tallHat' && buttonLever === 'button') {
       $('#one').show();
       $('#two').hide();
       $('#three').hide();
       $('#four').hide();
-    } else if (soup === 'tomatoSoup' && hat === 'shortHat') {
+      $('#five').hide();
+      $('#six').hide();
+      $('#seven').hide();
+      $('#eight').hide();
+    } else if (soup === 'tomatoSoup' && hat === 'tallHat' && buttonLever === 'lever') {
       $('#two').show();
       $('#one').hide();
       $('#three').hide();
       $('#four').hide();
-    } else if (soup === 'peaSoup' && hat === 'tallHat') {
+      $('#five').hide();
+      $('#six').hide();
+      $('#seven').hide();
+      $('#eight').hide();
+    } else if (soup === 'tomatoSoup' && hat === 'shortHat' && buttonLever === 'button') {
       $('#three').show();
       $('#one').hide();
       $('#two').hide();
       $('#four').hide();
-    } else if (soup === 'peaSoup' && hat === 'shortHat') {
+      $('#five').hide();
+      $('#six').hide();
+      $('#seven').hide();
+      $('#eight').hide();
+    } else if (soup === 'tomatoSoup' && hat === 'shortHat' && buttonLever === 'lever') {
       $('#four').show();
       $('#one').hide();
       $('#two').hide();
       $('#three').hide();
+      $('#five').hide();
+      $('#six').hide();
+      $('#seven').hide();
+      $('#eight').hide();
+    } else if (soup === 'peaSoup' && hat === 'tallHat' && buttonLever === 'button') {
+      $('#five').show();
+      $('#one').hide();
+      $('#two').hide();
+      $('#three').hide();
+      $('#four').hide();
+      $('#six').hide();
+      $('#seven').hide();
+      $('#eight').hide();
+    } else if (soup === 'peaSoup' && hat === 'tallHat' && buttonLever === 'lever') {
+      $('#six').show();
+      $('#one').hide();
+      $('#two').hide();
+      $('#three').hide();
+      $('#four').hide();
+      $('#five').hide();
+      $('#seven').hide();
+      $('#eight').hide();
+    } else if (soup === 'peaSoup' && hat === 'shortHat' && buttonLever === 'button') {
+      $('#seven').show();
+      $('#one').hide();
+      $('#two').hide();
+      $('#three').hide();
+      $('#four').hide();
+      $('#five').hide();
+      $('#six').hide();
+      $('#eight').hide();
+    } else if (soup === 'peaSoup' && hat === 'shortHat' && buttonLever === 'lever') {
+      $('#eight').show();
+      $('#one').hide();
+      $('#two').hide();
+      $('#three').hide();
+      $('#four').hide();
+      $('#five').hide();
+      $('#six').hide();
+      $('#seven').hide();
     }
 
 
