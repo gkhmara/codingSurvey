0bf3c5bc5097a979b2d0cef3e9a2fcacabf1c5a5
Greg Khmara <gkhmara@gmail.com>
2022-01-28 17:52:15 -0800

    Create second question for coding survey.

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,29 @@
 //USER INTERFACE LOGIC
 $(document).ready(function(){
   $("#survey").submit(function(event){
-    event.preventDefault();
+    
     const nameInput=$("input#name").val();
+    const soup = $("#soup").val();
 
     $(".name").text(nameInput);
+    // $(".soup").text(soup);
+
+    if (soup === 'tomatoSoup') {
+      $("#tomato").show();
+      $("#pea").hide();
+    } else if (soup === 'peaSoup') {
+      $("#pea").show();
+      $("#tomato").hide();
+    }
+
+
+    //TEST CODE
+    alert(nameInput);
+    alert(soup);
 
     $("#results").show();
+
+    event.preventDefault();
   });
 });
+
