3cdeadaab7eecc3850639797cb2718510fecc187
Greg Khmara <gkhmara@gmail.com>
2022-01-28 18:25:36 -0800

    Create second question and final result test output.

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,13 @@ $(document).ready(function(){
   $("#survey").submit(function(event){
     
     const nameInput=$("input#name").val();
-    const soup = $("#soup").val();
+    const soup = $("select#soup").val();
+    const hat = $("select#hat").val();
 
+    //Name Question
     $(".name").text(nameInput);
-    // $(".soup").text(soup);
 
+    //Soup Question
     if (soup === 'tomatoSoup') {
       $("#tomato").show();
       $("#pea").hide();
@@ -18,10 +20,44 @@ $(document).ready(function(){
       $("#tomato").hide();
     }
 
+    //Hat Question
+    if (hat === 'tallHat') {
+      $("#tallHat").show();
+      $("#shortHat").hide();
+    } else if (hat === 'shortHat') {
+      $("#shortHat").show();
+      $("#tallHat").hide();
+    }
+
+    //FINAL RESULTS TEST
+    if (soup === 'tomatoSoup' && hat === 'tallHat') {
+      $('#one').show();
+      $('#two').hide();
+      $('#three').hide();
+      $('#four').hide();
+    } else if (soup === 'tomatoSoup' && hat === 'shortHat') {
+      $('#two').show();
+      $('#one').hide();
+      $('#three').hide();
+      $('#four').hide();
+    } else if (soup === 'peaSoup' && hat === 'tallHat') {
+      $('#three').show();
+      $('#one').hide();
+      $('#two').hide();
+      $('#four').hide();
+    } else if (soup === 'peaSoup' && hat === 'shortHat') {
+      $('#four').show();
+      $('#one').hide();
+      $('#two').hide();
+      $('#three').hide();
+    }
+
+
 
     //TEST CODE
     alert(nameInput);
     alert(soup);
+    alert(hat);
 
     $("#results").show();
 
