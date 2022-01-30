b4449512e7f6abd197cbbeac0df5f8b4f18cee0c
Greg Khmara <gkhmara@gmail.com>
2022-01-29 17:14:49 -0800

    Create final branch testing for final results. Also checks that user enters name.

--- js/scripts.js
+++ js/scripts.js
@@ -12,162 +12,53 @@ $(document).ready(function(){
     const grapes = $("select#grapes").val();
 
     //Name Question
-    $(".name").text(nameInput);
-
-    //Question One
-    if (soup === 'tomatoSoup') {
-      $("#tomato").show();
-      $("#pea").hide();
-    } else if (soup === 'peaSoup') {
-      $("#pea").show();
-      $("#tomato").hide();
-    }
-
-    //Question Two
-    if (hat === 'tallHat') {
-      $("#tallHat").show();
-      $("#shortHat").hide();
-    } else if (hat === 'shortHat') {
-      $("#shortHat").show();
-      $("#tallHat").hide();
-    }
-
-    //Question Three
-    if (buttonLever === 'button') {
-      $("#button").show();
-      $("#lever").hide();
-    } else if (buttonLever === 'lever') {
-      $("#lever").show();
-      $("#button").hide();
-    }
-
-    //Question Four
-    if (food === 'hunt') {
-      $("#hunt").show();
-      $("#farm").hide();
-    } else if (food === 'farm') {
-      $("#farm").show();
-      $("#hunt").hide();
-    }
-
-    //Question Five
-    if (grapes === 'green') {
-      $("#green").show();
-      $("#purple").hide();
-    } else if (grapes === 'purple') {
-      $("#purple").show();
-      $("#green").hide();
-    }
-
-     //ACTUAL FINAL OUTPUT
-     if (soup === 'tomatoSoup') {
-      $('#one').show();
-      $('#two').hide();
-      $('#three').hide();
-    } else if (soup === 'peaSoup') {
-      $('#two').show();
-      $('#one').hide();
-      $('#three').hide();
-    } else if (hat === 'tallHat') {
-      $('#three').show();
+    if (Boolean(nameInput) === false) {
+      $("#nameOutput").hide();
+      $("#error").show();
       $('#one').hide();
       $('#two').hide();
-    } else if (hat === 'shortHat') {
-      $('#one').show();
-      $('#two').hide();
       $('#three').hide();
-    } else if (buttonLever === 'button') {
-      $('#two').show();
-      $('#one').hide();
-      $('#three').hide();
-    } else if (buttonLever === 'lever') {
-      $('#three').show();
-      $('#one').hide();
-      $('#two').hide();
-    } else if (food === 'hunt')
+    } else if (Boolean(nameInput) === true){
+      $(".name").text(nameInput);
+      $("#error").hide();
+      $("#nameOutput").show();
 
-    //FINAL RESULTS TEST
-    // if (soup === 'tomatoSoup' && hat === 'tallHat' && buttonLever === 'button') {
-    //   $('#one').show();
-    //   $('#two').hide();
-    //   $('#three').hide();
-    //   $('#four').hide();
-    //   $('#five').hide();
-    //   $('#six').hide();
-    //   $('#seven').hide();
-    //   $('#eight').hide();
-    // } else if (soup === 'tomatoSoup' && hat === 'tallHat' && buttonLever === 'lever') {
-    //   $('#two').show();
-    //   $('#one').hide();
-    //   $('#three').hide();
-    //   $('#four').hide();
-    //   $('#five').hide();
-    //   $('#six').hide();
-    //   $('#seven').hide();
-    //   $('#eight').hide();
-    // } else if (soup === 'tomatoSoup' && hat === 'shortHat' && buttonLever === 'button') {
-    //   $('#three').show();
-    //   $('#one').hide();
-    //   $('#two').hide();
-    //   $('#four').hide();
-    //   $('#five').hide();
-    //   $('#six').hide();
-    //   $('#seven').hide();
-    //   $('#eight').hide();
-    // } else if (soup === 'tomatoSoup' && hat === 'shortHat' && buttonLever === 'lever') {
-    //   $('#four').show();
-    //   $('#one').hide();
-    //   $('#two').hide();
-    //   $('#three').hide();
-    //   $('#five').hide();
-    //   $('#six').hide();
-    //   $('#seven').hide();
-    //   $('#eight').hide();
-    // } else if (soup === 'peaSoup' && hat === 'tallHat' && buttonLever === 'button') {
-    //   $('#five').show();
-    //   $('#one').hide();
-    //   $('#two').hide();
-    //   $('#three').hide();
-    //   $('#four').hide();
-    //   $('#six').hide();
-    //   $('#seven').hide();
-    //   $('#eight').hide();
-    // } else if (soup === 'peaSoup' && hat === 'tallHat' && buttonLever === 'lever') {
-    //   $('#six').show();
-    //   $('#one').hide();
-    //   $('#two').hide();
-    //   $('#three').hide();
-    //   $('#four').hide();
-    //   $('#five').hide();
-    //   $('#seven').hide();
-    //   $('#eight').hide();
-    // } else if (soup === 'peaSoup' && hat === 'shortHat' && buttonLever === 'button') {
-    //   $('#seven').show();
-    //   $('#one').hide();
-    //   $('#two').hide();
-    //   $('#three').hide();
-    //   $('#four').hide();
-    //   $('#five').hide();
-    //   $('#six').hide();
-    //   $('#eight').hide();
-    // } else if (soup === 'peaSoup' && hat === 'shortHat' && buttonLever === 'lever') {
-    //   $('#eight').show();
-    //   $('#one').hide();
-    //   $('#two').hide();
-    //   $('#three').hide();
-    //   $('#four').hide();
-    //   $('#five').hide();
-    //   $('#six').hide();
-    //   $('#seven').hide();
-    // }
+      //ACTUAL FINAL OUTPUT
+      if (food === 'farm') {
+        $('#three').show();
+        $('#one').hide();
+        $('#two').hide();
+      } else if (soup === 'peaSoup') {
+        $('#two').show();
+        $('#one').hide();
+        $('#three').hide();
+      } else if (hat === 'tallHat') {
+        $('#three').show();
+        $('#one').hide();
+        $('#two').hide();
+      } else if (buttonLever === 'button') {
+        $('#one').show();
+        $('#two').hide();
+        $('#three').hide();
+      } else if (grapes === 'purple') {
+        $('#two').show();
+        $('#one').hide();
+        $('#three').hide();
+      } else if (grapes === 'green') {
+        $('#one').show();
+        $('#two').hide();
+        $('#three').hide();
+      } else {
+        $('#one').hide();
+        $('#two').hide();
+        $('#three').hide();
+      }
+    }
 
-    //TEST CODE
-    alert(nameInput);
-    alert(soup);
-    alert(hat);
-    alert(buttonLever);
-    alert(food);
-    alert(grapes);
+    //TESTCODE
+    alert(typeof nameInput);
+    alert(nameInput.length)
+    alert(Boolean(nameInput))
 
     $("#results").show();
 
