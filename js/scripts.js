78014f1ff10e8a94504355f8bbbc494aacc1eb88
Greg Khmara <gkhmara@gmail.com>
2022-01-29 20:13:50 -0800

    fix indentation on all files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-//BUSINESS LOGIC
-
-//USER INTERFACE LOGIC
 $(document).ready(function(){
   $("#survey").submit(function(event){
     
@@ -18,43 +15,60 @@ $(document).ready(function(){
       $('#one').hide();
       $('#two').hide();
       $('#three').hide();
+      $('#four').hide();
+      $('#five').hide();
     } else if (Boolean(nameInput) === true){
       $(".name").text(nameInput);
       $("#error").hide();
       $("#nameOutput").show();
 
-      //ACTUAL FINAL OUTPUT
-      if (food === 'farm') {
-        $('#three').show();
-        $('#one').hide();
-        $('#two').hide();
-      } else if (soup === 'peaSoup') {
-        $('#two').show();
-        $('#one').hide();
-        $('#three').hide();
-      } else if (hat === 'tallHat') {
-        $('#three').show();
-        $('#one').hide();
-        $('#two').hide();
-      } else if (buttonLever === 'button') {
+      //Final Output
+      if (soup === 'peaSoup') {
         $('#one').show();
         $('#two').hide();
         $('#three').hide();
-      } else if (grapes === 'purple') {
+        $('#four').hide();
+        $('#five').hide();
+      } else if (hat === 'tallHat') {
         $('#two').show();
         $('#one').hide();
         $('#three').hide();
+        $('#four').hide();
+        $('#five').hide();
+      } else if (buttonLever === 'button') {
+        $('#three').show();
+        $('#one').hide();
+        $('#two').hide();
+        $('#four').hide();
+        $('#five').hide();
+      } else if (food === 'farm') {
+        $('#four').show();
+        $('#one').hide();
+        $('#two').hide();
+        $('#three').hide();
+        $('#five').hide();
+      } else if (grapes === 'purple') {
+        $('#five').show();
+        $('#one').hide();
+        $('#two').hide();
+        $('#three').hide();
+        $('#four').hide();
       } else if (grapes === 'green') {
         $('#one').show();
         $('#two').hide();
         $('#three').hide();
+        $('#four').hide();
+        $('#five').hide();
       } else {
         $('#one').hide();
         $('#two').hide();
         $('#three').hide();
+        $('#four').hide();
+        $('#five').hide();
       }
     }
 
+    //Show Final Results
     $("#results").show();
 
     event.preventDefault();
